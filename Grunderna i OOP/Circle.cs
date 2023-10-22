7f013a632fc1a454f1f5fd8e504d45a535936636
mlvestlund <mariavestlund@yahoo.se>
2023-10-22 18:46:58 +0200

    Add project files.

---
+++ Grunderna i OOP/Circle.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Grunderna_i_OOP
+{
+    internal class Circle
+    {
+        public double Radius {  get; }
+        public double Area { get; }
+        public double Circumference { get; }
+        public double VolumeIfItWereASphere { get; }
+
+        public Circle(double radius) //Constructor that gives radius a value
+        {
+            Radius = radius;
+            Area = GetArea();
+            Circumference = GetCircumference();
+            VolumeIfItWereASphere = GetVolumeIfItWereASphere();
+        }
+
+        public double GetArea() //method that gives the area of given radius
+        {
+            return Math.PI * Math.Pow(Radius, 2);
+        }
+
+        public double GetCircumference()
+        {
+            return Math.PI * Radius * 2;
+        }
+
+        public double GetVolumeIfItWereASphere()
+        {
+            return (4 * Math.PI * Math.Pow(Radius, 3)) / 3;
+        }
+    }
+}
